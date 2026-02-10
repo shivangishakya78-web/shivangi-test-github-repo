b1312f53ed3bb7e9834f3b1350d842f736ca9386
EC2 Default User <ec2-user@ip-192-168-1-80.ap-south-1.compute.internal>
2026-02-10 04:33:56 +0000

    Initial LAMP setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Simple PHP Script
+
+echo "Hello, Yeh meri pehli PHP Script hai!";
+
+// Variable declare karna
+$name = "Shivangi";
+$age = 22;
+
+// Output display karna
+echo "<br>";
+echo "Mera naam hai: " . $name;
+echo "<br>";
+echo "Meri age hai: " . $age;
+?>
